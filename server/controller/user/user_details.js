9473afb7c322d278b1968f7281421103ecb99709
Themos Daskalopoulos <themosd@gmail.com>
2023-03-17 00:18:47 +0200

    user_details to client side

--- server/controller/user/user_details.js
+++ server/controller/user/user_details.js
@@ -9,32 +9,61 @@ var request = require('request'); // "Request" library
 router.get('/', (req, res) => {
     // get token from session
     token = req.session.token;
-    console.log("MY TOKEN: " + token);
-    var options = {
+    // console.log("MY TOKEN: " + token);
+
+    var options_artists = {
         url: 'https://api.spotify.com/v1/me/top/artists?'  +
         querystring.stringify({
-            limit: 5,
+            limit: 5, 
             offset: 0,
-            time_range: "short_term"
+            time_range: "short_term" // TODO add time range as variable
+        }),
+    
+        headers: { 'Authorization': 'Bearer ' + token },
+        json: true
+      };
+
+      var options_tracks = {
+        url: 'https://api.spotify.com/v1/me/top/tracks?'  +
+        querystring.stringify({
+            limit: 5, 
+            offset: 0,
+            time_range: "short_term" // TODO add time range as variable
         }),
     
         headers: { 'Authorization': 'Bearer ' + token },
         json: true
       };
     
-    request.get(options, function(error, response, body) {
-        var artist = [];
+    request.get(options_artists, function(error, response, body) {
+        var artists = [];
         // var songName = [];
 
         for (i=0; i<5;i++) {
-            artist[i] = body.items[i].name;
-            console.log(artist[i]);
+            artists[i] = body.items[i].name;
+            // console.log(artists[i]);
         }
-
+        res.json(artists)
         // console.log(body.items[1].name);
         // console.log(response.statusCode);
         
       }); 
+
+// TODO promise.all() to get both requests before sending through res.json()
+
+    // request.get(options_tracks, function(error, response, body) {
+    //   var artist = [];
+    //   // var songName = [];
+
+    //   for (i=0; i<5;i++) {
+    //       artist[i] = body.items[i].name;
+    //       console.log(artist[i]);
+    //   }
+    //   res.json(artist)
+    //   // console.log(body.items[1].name);
+    //   // console.log(response.statusCode);
+      
+    // }); 
 
   })
 
--- server/static/scripts/dashboard.js
+++ server/static/scripts/dashboard.js
@@ -1,24 +1,55 @@
 // npm i --save-dev @types/jquery
 
-console.log("IN dashboard.js ")
+let dashboard_data = [];
+
 
 // Retrieve backend data
-var xhr = new XMLHttpRequest(); // get HTML dashboard from '
-xhr.open('GET', '/tokenise');
-xhr.onload = function() {
-  if (xhr.status === 200) {
-    let data = xhr.responseText;
-    let dt = JSON.parse(data)
+let xhr_tokenise = new XMLHttpRequest();
+xhr_tokenise.open('GET', '/tokenise');
+xhr_tokenise.onload = function() {
+  if (xhr_tokenise.status === 200) {
 
-    loadDashboard(dt)
+    let data = xhr_tokenise.responseText;
+    let data_tokenise = JSON.parse(data);
+
+    dashboard_data[0] = data_tokenise.name;
   }
 };
-xhr.send();
+xhr_tokenise.send();
 
 
-function loadDashboard(data) {
 
-    let name_field = document.getElementById("name_field");
-    
-    name_field.innerHTML =  "Name: <i>" + data.name + "</i>";
+let xhr_user_details = new XMLHttpRequest(); 
+xhr_user_details.open('GET', '/user_details');
+xhr_user_details.onload = function() {
+  if (xhr_user_details.status === 200) {
+
+    let data = xhr_user_details.responseText;
+    let data_user_details = JSON.parse(data)
+
+    for (let i=0;i<5;i++) {
+      dashboard_data[i+1] = data_user_details[i]
+    }
+    console.log(dashboard_data[0])
+    loadDashboard(dashboard_data)
+  }
+};
+xhr_user_details.send();
+
+
+// display data in client side
+
+
+
+function loadDashboard(dashboard_data) {
+  console.log(dashboard_data[0])
+  // show user name
+  let name_field = document.getElementById("name_field");
+  name_field.innerHTML =  "Name: <i>" + dashboard_data[0] + "</i>";
+
+  let list = document.getElementById("items_list");
+  for (var i = 0; i < 5; i++) {
+    list.children[i].innerHTML = dashboard_data[i+1]
+    // console.log(list.children[i]);
+}
 }
