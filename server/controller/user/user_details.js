1d8c62fa3b639f72543759bdb88a69067d10339e
Themos Daskalopoulos <themosd@gmail.com>
2023-04-01 22:48:59 +0300

    items selector done

--- server/controller/user/user_details.js
+++ server/controller/user/user_details.js
@@ -6,17 +6,19 @@ var request = require('request'); // "Request" library
 
 
 // get current user's top items (songs, artists etc.)
-router.get('/', (req, res) => {
+router.get('/?type/:type/time_range/:time_range', (req, res) => {
     // get token from session
     token = req.session.token;
-    // console.log("MY TOKEN: " + token);
- 
+
+    var item_type = req.params.type;// songs or artists 
+    var item_time_range = req.params.time_range; // time range
+
     var options_artists = {
         url: 'https://api.spotify.com/v1/me/top/artists?'  +
         querystring.stringify({
             limit: 5, 
             offset: 0,
-            time_range: "short_term" // TODO add time range as variable
+            time_range: item_time_range // TODO add time range as variable
         }),
     
         headers: { 'Authorization': 'Bearer ' + token },
@@ -28,42 +30,36 @@ router.get('/', (req, res) => {
         querystring.stringify({
             limit: 5, 
             offset: 0,
-            time_range: "short_term" // TODO add time range as variable
+            time_range: item_time_range // TODO add time range as variable
         }),
     
         headers: { 'Authorization': 'Bearer ' + token },
         json: true
       };
-    
-    request.get(options_artists, function(error, response, body) {
-        var artists = [];
+
+    if (item_type == "artists") {
+      var options = options_artists
+    } else if (item_type == "tracks") {
+      var options = options_tracks
+    } else {return res.status(400).send({ // ERROR
+      message: 'This is an error!'
+        });
+    } 
+
+    request.get(options, function(error, response, body) {
+        var items = [];
         // var songName = [];
 
         for (i=0; i<5;i++) {
-            artists[i] = body.items[i].name;
+          items[i] = body.items[i].name;
             // console.log(artists[i]);
         }
-        res.json(artists)
+        res.json(items)
         // console.log(body.items[1].name);
         // console.log(response.statusCode);
         
       }); 
 
-// TODO promise.all() to get both requests before sending through res.json()
-
-    // request.get(options_tracks, function(error, response, body) {
-    //   var artist = [];
-    //   // var songName = [];
-
-    //   for (i=0; i<5;i++) {
-    //       artist[i] = body.items[i].name;
-    //       console.log(artist[i]);
-    //   }
-    //   res.json(artist)
-    //   // console.log(body.items[1].name);
-    //   // console.log(response.statusCode);
-      
-    // }); 
 
   })
 
--- server/static/scripts/dashboard.js
+++ server/static/scripts/dashboard.js
@@ -1,47 +1,50 @@
-// npm i --save-dev @types/jquery
+function call_API() {
+  
+  let dashboard_data = [];
 
-let dashboard_data = [];
+  // Retrieve backend data for user
+  let xhr_tokenise = new XMLHttpRequest();
+  xhr_tokenise.open('GET', '/tokenise');
+  xhr_tokenise.onload = function() {
+    if (xhr_tokenise.status === 200) {
 
-// Retrieve backend data
-let xhr_tokenise = new XMLHttpRequest();
-xhr_tokenise.open('GET', '/tokenise');
-xhr_tokenise.onload = function() {
-  if (xhr_tokenise.status === 200) {
+      let data = xhr_tokenise.responseText;
+      let data_tokenise = JSON.parse(data);
 
-    let data = xhr_tokenise.responseText;
-    let data_tokenise = JSON.parse(data);
+      dashboard_data[0] = data_tokenise.name;
+      dashboard_data[1] = data_tokenise.profile_pic;
+      dashboard_data[2] = data_tokenise.followers;
 
-    dashboard_data[0] = data_tokenise.name;
-    dashboard_data[1] = data_tokenise.profile_pic;
-    dashboard_data[2] = data_tokenise.followers;
-
-  }
-};
-xhr_tokenise.send();
-
-
-
-let xhr_user_details = new XMLHttpRequest(); 
-xhr_user_details.open('GET', '/user_details');
-xhr_user_details.onload = function() {
-  if (xhr_user_details.status === 200) {
-
-    let data = xhr_user_details.responseText;
-    let data_user_details = JSON.parse(data)
-    for (let i=0;i<5;i++) {
-      dashboard_data[i+3] = data_user_details[i]
     }
+  };
+  xhr_tokenise.send();
 
-    loadDashboard(dashboard_data)
-  }
-};
-xhr_user_details.send();
+  // get UI values from item types & time range
+  let type = document.getElementById("item_types").value;
+  let time_range = document.getElementById("item_time_range").value;
+
+
+  // Retrieve backend data for user items
+  let xhr_user_details = new XMLHttpRequest(); 
+  xhr_user_details.open('GET', '/user_details/type/' + type + '/time_range/' + time_range);
+  xhr_user_details.onload = function() {
+    if (xhr_user_details.status === 200) {
+
+      let data = xhr_user_details.responseText;
+      let data_user_details = JSON.parse(data)
+      for (let i=0;i<5;i++) {
+        dashboard_data[i+3] = data_user_details[i]
+      }
+
+      loadDashboard(dashboard_data)
+    }
+  };
+  xhr_user_details.send();
+}
+
 
 
 // display data in client side
-
-
-
 function loadDashboard(dashboard_data) {
 
   // show user name
@@ -64,7 +67,7 @@ function loadDashboard(dashboard_data) {
 }
 
 
-
+// TODO
 function getStatistics() {
   console.log("inside")
   let xhr_statistics = new XMLHttpRequest();
